9de852f421497b1cf84bc9977628a6e5052d0a09
Muhammad Shafqat <mshafqat.developer@gmail.com>
2025-08-16 21:10:05 +0500

    db changed and docker file added

--- Libs/Database.js
+++ Libs/Database.js
@@ -2,10 +2,7 @@ import mongoose from 'mongoose';
 
 const connectToDb = async () => {
     try {
-        await mongoose.connect("mongodb+srv://msr:msr1020@sm.xss0zur.mongodb.net/?retryWrites=true&w=majority", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+        await mongoose.connect("mongodb://localhost:27017/next-crud")
         console.log("Connected to database Successfully")
     } catch (error) {
         console.log("Error connecting to database")
